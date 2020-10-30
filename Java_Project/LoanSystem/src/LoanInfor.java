6f052039f846e21f73b0ce3deb47fb0a477d42b3
ohMySunset <znfnznfnakqj@naver.com>
2020-10-30 17:58:22 +0900

    bitBank Project

---
+++ Java_Project/LoanSystem/src/LoanInfor.java
@@ -0,0 +1,18 @@
+
+public class LoanInfor {
+
+	String name;
+	String amount;
+	String loanPeriod;
+	Double interestRate;
+	int loanAmount;
+	int loanBalance;
+	int balance;
+	Double interest;
+	
+	
+	
+	
+	
+	
+}
