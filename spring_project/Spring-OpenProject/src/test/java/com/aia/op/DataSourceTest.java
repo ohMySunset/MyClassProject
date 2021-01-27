4cfd4accf192c725e05d06d875793fe8d8fd2724
ohMySunset <znfnznfnakqj@naver.com>
2021-01-27 10:11:10 +0900

    update : test spring & project erd - 20210127

---
+++ spring_project/Spring-OpenProject/src/test/java/com/aia/op/DataSourceTest.java
@@ -0,0 +1,60 @@
+package com.aia.op;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+//@Log4j
+public class DataSourceTest {
+	
+	@Autowired
+	//@Setter(onMethod_ = { @Autowired })
+	private DataSource datasource;
+	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testMybatis() {	
+		try(
+				SqlSession session = sqlSessionFactory.openSession(); 
+				Connection conn = session.getConnection(); ){	
+			
+		System.out.println(session);
+		System.out.println(conn);
+		
+		} catch (Exception e) {
+			fail(e.getMessage());
+			//e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try(Connection con = datasource.getConnection();){
+			//log.info(con);
+			System.out.println("!!! Connection :"+ con);
+		}catch(Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+}
